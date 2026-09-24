683c164d32a85221c57efe6765b967d27c024c56
Odysseesus <140325281+Odysseesus@users.noreply.github.com>
2026-09-24 21:46:59 +0700

    tạo field và gán setter, gette.

--- src/model/VisitSegment.java
+++ src/model/VisitSegment.java
@@ -10,6 +10,67 @@ import java.time.LocalDateTime;
  *
  * @author ACER
  */
-public class VisitSegment {
-    
+public class VisitSegment{
+    private String departmentId;
+    private String doctorId;
+    private String examResults;
+    private String diagnosis;
+    private String testOrder;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public VisitSegment(String departmentId){
+        this.departmentId = departmentId;
+        this.startTime = LocalDateTime.now();
+    }
+
+    public String getDepartmentId()
+    {
+        return departmentId;
+    }
+    public String getDoctorId()
+    {
+        return doctorId;
+    }
+    public String getExamResults()
+    {
+        return examResults;
+    }
+    public String getDiagnosis()
+    {
+        return diagnosis;
+    }
+    public String getTestOrder()
+    {
+        return testOrder;
+    }
+    public LocalDateTime getStartTime()
+    {
+        return startTime;
+    }
+    public LocalDateTime getEndTime()
+    {
+        return endTime;
+    }
+
+    public void setDoctorId(String doctorId)
+    {
+        this.doctorId = doctorId;
+    }
+    public void setExamResults(String examResults)
+    {
+        this.examResults = examResults;
+    }
+    public void setDiagnosis(String diagnosis)
+    {
+        this.diagnosis = diagnosis;
+    }
+    public void setTestOrder(String testOrder)
+    {
+        this.testOrder = testOrder;
+    }
+    public void setEndTime(LocalDateTime endTime)
+    {
+        this.endTime = endTime;
+    }  
 }
